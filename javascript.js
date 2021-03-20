3742d7d05a1c0d1a4215b8a7f50d86c147777be1
hakanocal <hakanocaltr@gmail.com>
2021-03-20 23:05:38 +0300

    deneme

--- javascript.js
+++ javascript.js
@@ -10,22 +10,7 @@ function bodyOnLoad(){
     document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
     document.getElementById('insertedWordList').innerHTML += "takew" + "\n"; 
     document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "takwe" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "takew" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "tawke" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
-    document.getElementById('insertedWordList').innerHTML += "take" + "\n"; 
+    
 
 }
 // enter tuşuna tıklandığında send butonunu çalıştır
