3a8d3e2e9b6f889e4250dbb1d80d268fa357d4fb
hakanocal <hakanocaltr@gmail.com>
2021-03-26 19:33:59 +0300

    cookie denemesi

--- javascript.js
+++ javascript.js
@@ -456,3 +456,37 @@ if (userLang == "tr-TR" || userLang == "tr"){
 else{
     langEN();
 }
+
+// COOKIE
+function setCookie(cookieName,cookieValue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+  
+  function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+  
+function checkCookie() {
+    var isVisited = getCookie("isVisited");
+    if (!isVisited) {
+        alert("Welcome again");
+    } 
+    else {
+        setCookie("isVisited", true, 30);
+    }
+}
